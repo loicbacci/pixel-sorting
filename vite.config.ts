8a07a1bc53f7d3cbf523c49a38fa147edbce3558
Loïc Baccigalupi <loic.baccigalupi@gmail.com>
2026-06-08 16:13:41 +0200

    Setup pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,5 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [preact(), tailwindcss()],
+	base: process.env.NODE_ENV === 'production' ? '/pixel-sorting/' : '/',
 });
